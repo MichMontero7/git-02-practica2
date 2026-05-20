710e1f08115d08a7ede4ba5e064aef0f8af472f3
Fernanda Montero <20253ds039@utez.edu.mx>
2026-05-20 13:50:10 -0600

    [NEW] clase calculadora y constructor

---
+++ src/main/java/mx/edu/utez/press/Calculadora.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.press;
+
+public class Calculadora {
+
+    private double numero1;
+    private  double numero2;
+
+    public Calculadora (double numero1, double numero2) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
+
+}
